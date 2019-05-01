6956615232a2ccceb710e9656286fd518e363849
Kartik Sachdeva <kartiksach@Kartiks-MacBook-Pro.local>
2019-05-01 17:36:21 +0530

    Adding files to remote

---
+++ App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <AppContainer />
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> This is my Home screen </Text>
+      </View>
+    );
+  }
+}
+
+class ExploreScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Explore screen </Text>
+      </View>
+    );
+  }
+}
+
+class NotificationsScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff'}}>
+        <Text> This is my Notifications screen </Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Profile screen </Text>
+      </View>
+    );
+  }
+}
+
+const bottomTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="home" size={25} color={tintColor} />
+        )
+      }
+    },
+    Explore: {
+      screen: ExploreScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="comments" size={25} color={tintColor} />
+        )
+      }
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="user" size={25} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="search" size={25} color={tintColor} />
+        )
+      }
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: '#eb6e3d'
+    }
+  }
+);
+
+const AppContainer = createAppContainer(bottomTabNavigator);
