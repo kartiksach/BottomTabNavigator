0f5d665013f53d5e66e9e07a6555f55e5c41477e
Kartik Sachdeva <kartiksach@Kartiks-MacBook-Pro.local>
2019-05-01 17:45:28 +0530

    updating icon name

--- App.js
+++ App.js
@@ -73,7 +73,7 @@ const bottomTabNavigator = createBottomTabNavigator(
       screen: NotificationsScreen,
       navigationOptions: {
         tabBarIcon: ({ tintColor }) => (
-          <Icon name="user" size={25} color={tintColor} />
+          <Icon name="search" size={25} color={tintColor} />
         )
       }
     },
@@ -81,7 +81,7 @@ const bottomTabNavigator = createBottomTabNavigator(
       screen: ProfileScreen,
       navigationOptions: {
         tabBarIcon: ({ tintColor }) => (
-          <Icon name="search" size={25} color={tintColor} />
+          <Icon name="profile" size={25} color={tintColor} />
         )
       }
     },
